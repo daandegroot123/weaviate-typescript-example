203341fff9384a6e870a0a871ac80222e80f2a41
Daan de Groot <daandegroot44@gmail.com>
2023-04-09 19:51:54 +0200

    final version

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Testing vector databases
-A vector database that enables efficient similarity search and indexing of high-dimensional data. Vector databases are particularly useful for machine learning applications, where data points are often represented as high-dimensional vectors. These databases allow users to perform similarity searches based on the embeddings of the data points, making it easier to find relevant information based on content or features.
+# Weaviate vector database using TypeScript and OpenAI's embedder
+A vector database enables efficient similarity search and indexing of high-dimensional data. Vector databases are particularly useful for machine learning applications, where data points are often represented as high-dimensional vectors. These databases allow users to perform similarity searches based on the embeddings of the data points, making it easier to find relevant information based on content or features.
+
+In this examplary project, I have used [Weaviate](https://github.com/weaviate/weaviate) as a vector database and their integration with [OpenAI's embedder](https://platform.openai.com/docs/guides/embeddings) (`text-embedding-ada-002-v2`) to create and save vectors of a dataset. As example dataset, I have scraped all the Y Combinator from [their website](https://www.ycombinator.com/companies) (4000+ companies) and created vectors out of key datapoint (see `/examples/y-combinator/schema.json` for more details). I have also uploaded the dataset to [Kaggle](https://www.kaggle.com/datasets/daandegrote/y-combinator-companies-until-w23).
+
+As a second example, I have downloaded a Lord of the Rings character dataset from [Kaggle, created by Paul Mooney](https://www.kaggle.com/datasets/paultimothymooney/lord-of-the-rings-data).
 
 ## Installation
 Create a .env file in the root directory and add the variables: OPENAI_APIKEY and DATA_PATH. The DATA_PATH variable should be the path to the data directory:
@@ -24,8 +28,27 @@ docker-compose up -d --build
 ```
 
 ## Usage
-Run the script using (requires `ts-node` npm package):
+Populate the Weaviate database (make sure Docker is running). You can use one of the example datasets provided in the `/examples` directory (currently `y-combinator` and `lotr-character`). 
 
+Running `populate` will create a Weaviate database with the data from the example dataset (and remove any existing data with the same class). _I paid ~$0.20 in OpenAI usage to populate the Y Combinator dataset_
 ```
-ts-node index.ts  <example-name>
+ts-node index.ts populate <example-name>
 ```
+
+If succesful, you can easily view and query your data via the Weaviate GraphQL API. You can use the [Apollo GraphQL Sandbox](https://studio.apollographql.com/sandbox/explorer) to explore the data and run queries. Just enter the graph url (`http://localhost:8080/v1/graphql`).
+
+### Visualize your data
+If you want to have some extra fun, you can use [Tensorflow's Embedding Projector](https://projector.tensorflow.org/) to visualize your data. To get the .tsv files required use the following command (will place them inside of the `/example/<example-name>` folder):
+```
+ts-node index.ts tsv <example-name>
+```
+
+These can be uploaded to the website to visualize your data.
+
+## Add your own data
+To create your own data, follow on of the examples:
+1. Create a new folder inside of `/examples` with the name of your dataset.
+2. Create a `schema.json` file in the root of your dataset folder. This file should contain the schema of your data.
+3. Create a `data.json` file in the root of your dataset folder. This file should contain the data of your dataset.
+4. Run `ts-node index.ts populate <your-dataset-name>` to populate the database with your data.
+
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import weaviate, { WeaviateClient, ObjectsBatcher } from 'weaviate-ts-client';
-import csv from "csvtojson";
+import { TSV, Parser } from 'tsv';
 import fs from 'fs';
 
 const client: WeaviateClient = weaviate.client({
@@ -8,11 +8,14 @@ const client: WeaviateClient = weaviate.client({
 });
 
 async function deleteClass(className: string) {
+  // TODO: check if class exists first
+
+
   client.schema.classDeleter().withClassName(className).do().then((res: any) => {
     //console.log(res);
   })
   .catch((err: Error) => {
-    console.error(err)
+    console.error('Error while deleting class', err)
   });;
 }
 
@@ -33,16 +36,18 @@ async function populateData(example: string) {
   // We are repopulating all the data, thus we remove existing data (if any) first
   deleteClass(className);
   createClass(schemaObj);
+
   // Get the data from the data.json file
   const { data } = JSON.parse(fs.readFileSync('./examples/' + example + '/data.json', 'utf8'));
 
   // Prepare a batcher
   let batcher: ObjectsBatcher = client.batch.objectsBatcher();
   let counter: number = 0;
-  let batchSize: number = 10;
+  let batchSize: number = 100;
   let batchNumber = 1;
 
-  data.forEach((dataPoint: any) => {
+  // Loop through the data
+  for (const dataPoint of data) {
 
     // Create object properties from schema (make sure the property names match the schema)
     const properties: any = {};
@@ -56,28 +61,27 @@ async function populateData(example: string) {
       properties: properties,
     }
 
-    // add the object to the batch queue
+    // Add the object to the batch queue
     batcher = batcher.withObject(obj);
 
     // When the batch counter reaches batchSize, push the objects to Weaviate
     if (counter++ == batchSize) {
 
-      batchNumber += 1
-
-      // flush the batch queue
-      batcher
+      // Flush the batch queue
+      await batcher
       .do()
       .catch((err: Error) => {
         console.error(err)
       });
 
-      console.log('batchNumber', batchNumber)
+      console.log('Batch ' + batchNumber + '/' + Math.ceil(data.length / batchSize) + ' done. (' + data.length + ' objects in total)');
+      batchNumber++
 
-      // restart the batch queue
+      // Restart the batch queue
       counter = 0;
       batcher = client.batch.objectsBatcher();
     }
-  });
+  }
 
   // Flush the remaining objects
   batcher
@@ -90,94 +94,66 @@ async function populateData(example: string) {
   });
 }
 
-function main() {
-  // populateData(process.argv[2]);
-  getAllVectors();
-}
+async function createTsvFiles(example: string) {
+  const { data } = JSON.parse(fs.readFileSync('./examples/' + example + '/data.json', 'utf8'));
+  const schema = JSON.parse(fs.readFileSync('./examples/' + example + '/schema.json', 'utf8'));
+  const className = schema.class;
 
-async function similarText(text: string) {
-  client.graphql
-  .get()
-  .withClassName('Character')
-  .withFields('birth death gender hair height name race realm spouse _additional {certainty}')
-  .withNearText({concepts: [text]})
-  .withLimit(5)
-  .do()
-  .then((res: any) => {
-    console.log(JSON.stringify(res, null, 2))
-  })
-  .catch((err: Error) => {
-    console.error(err)
-  });
-}
+  // create a string of all schema properties seperated by a space
+  const schemaProperties = schema.properties.map((property: any) => {
+    return property.name
+  }).join(' ');
 
-//similarText('Bilbo Baggins')
+  const allVectors = await getAllVectors(className, schemaProperties, data.length)
 
-import { OpenAIApi, Configuration } from 'openai';
-
-async function openAiVec(text: string) {
-  const configuration = new Configuration({
-    apiKey: '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',
+  // remove newlines from the data
+  allVectors.forEach((dataPoint: any) => {
+    Object.keys(dataPoint).forEach((key: string) => {
+      if (typeof dataPoint[key] === 'string') {
+        dataPoint[key] = dataPoint[key].replace(/(\r\n|\n|\r)/gm, ' ');
+      }
+    });
   });
   
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createEmbedding({
-    model: "text-embedding-ada-002",
-    input: text,
+  const mappedVectors = allVectors.map((vector: any) => {
+    return vector._additional.vector
+  })
+
+  fs.writeFileSync('./examples/' + example + '/mapped.json', JSON.stringify(mappedVectors))
+  const makeTsv = JSON.parse(fs.readFileSync('./examples/' + example + '/mapped.json', 'utf8'))
+  const vectors = new Parser("\t", { header: false }).stringify(makeTsv)
+  fs.writeFileSync('./examples/' + example + '/vectors.tsv', vectors)
+
+  allVectors.forEach((dataPoint: any) => {
+    delete dataPoint._additional
   });
 
-  //console.log(response.data.data[0].embedding)
-  fs.writeFileSync('embedding.json', JSON.stringify(response.data.data[0].embedding, null, 2));
-  return response.data.data[0].embedding;
+  const metadata = TSV.stringify(allVectors)
+  fs.writeFileSync('./examples/' + example + '/metadata.tsv', metadata)
 }
 
-async function similarVector(vector: number[]) {
-  client.graphql
+async function getAllVectors(className: string, properties: string, limit: number) {
+  const response = await client.graphql
   .get()
-  .withClassName('Character')
-  .withFields('birth death gender hair height name race realm spouse _additional {certainty}')
-  .withNearVector({ vector })
-  .withLimit(5)
+  .withClassName(className)
+  .withFields( properties + ' _additional {vector}' )
+  .withLimit(limit)
   .do()
-  .then((res: any) => {
-    console.log(JSON.stringify(res, null, 2))
-  })
   .catch((err: Error) => {
     console.error(err)
   });
+
+  if ( response ) {
+    return response.data.Get[className]
+  }
 }
 
-async function start(text: string) {
-  await openAiVec(text)
-  similarVector(JSON.parse(fs.readFileSync('embedding.json', 'utf8')))
-}
-
-//start('Wizard')
-
-import { TSV } from 'tsv';
-
-async function createTsv() {
-  const characters = await csv().fromFile('./lotr_characters.csv');
-
-  const meta = TSV.stringify(characters)
-  fs.writeFileSync('meta.tsv', meta)
-}
-
-//createTsv()
-
-async function getAllVectors() {
-  client.graphql
-  .get()
-  .withClassName('Company')
-  .withFields('name')
-  .withLimit(5000)
-  .do()
-  .then((res: any) => {
-    console.log(res.data.Get.Company.length)
-  })
-  .catch((err: Error) => {
-    console.error(err)
-  });
+function main() {
+  if ( process.argv[2] == 'populate') {
+    populateData(process.argv[3]);
+  } else if ( process.argv[2] == 'tsv') {
+    createTsvFiles(process.argv[3]);
+  }
 }
 
 main();
